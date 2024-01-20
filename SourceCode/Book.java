951ef56b74f39ae6e994db201292b84228c58bda
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2024-01-20 23:20:10 +0630

    Doc Book and BookType

---
+++ SourceCode/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+
+    public final String name;
+    public final double price;
+
+    // reduce the variable of group for dependency
+    public final BookType type;
+
+    public Book(String name, double price, BookType type) {
+        this.name = name;
+        this.price = price;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public BookType getType() {
+        return type;
+    }
+
+}
